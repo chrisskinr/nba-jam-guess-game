eebcc9e4017b5de47cc39dce10387f081352e648
Christopher Skinner <christopherskinner@Mac.localdomain>
2025-04-25 23:41:44 -0500

    v12: 10 new centering methods from architect council

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,59 @@
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white p-8 space-y-8">
+    <div className="min-h-screen bg-black text-white text-xl space-y-10 p-10">
       
-      {/* Architect 1 - Flexbox Center */}
-      <div className="min-h-[200px] flex justify-center items-center border-2 border-red-500">
-        <div>🏀 Flexbox Center</div>
+      {/* 1. Tailwind Flexbox + h-screen (fixed) */}
+      <div className="h-screen flex justify-center items-center border-2 border-red-500">
+        <div>🏀 1. Flexbox (h-screen)</div>
       </div>
 
-      {/* Architect 2 - Grid Center */}
-      <div className="min-h-[200px] grid place-items-center border-2 border-green-500">
-        <div>🏀 Grid Place-Items Center</div>
+      {/* 2. Tailwind Grid + h-screen */}
+      <div className="h-screen grid place-items-center border-2 border-green-500">
+        <div>🏀 2. Grid Center (h-screen)</div>
       </div>
 
-      {/* Architect 3 - Margin Auto */}
-      <div className="min-h-[200px] border-2 border-blue-500 flex items-start">
-        <div style={{ margin: "0 auto", marginTop: "40px", width: "fit-content" }}>
-          🏀 Margin Auto Hack
-        </div>
+      {/* 3. Flex with explicit width/height on parent + margin auto */}
+      <div className="border-2 border-blue-500" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div style={{ width: "fit-content", margin: "0 auto" }}>🏀 3. Inline Margin Auto (fixed height)</div>
       </div>
 
-      {/* Architect 4 - Absolute Position */}
-      <div className="relative min-h-[200px] border-2 border-yellow-500">
+      {/* 4. Absolute with fixed height + relative wrapper */}
+      <div className="relative border-2 border-yellow-500" style={{ height: "100vh" }}>
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          🏀 Absolute Transform Center
+          🏀 4. Absolute + Transform (fixed wrapper)
         </div>
       </div>
 
-      {/* Architect 5 - Flexbox Column */}
-      <div className="min-h-[200px] flex flex-col justify-center items-center border-2 border-pink-500">
-        <div>🏀 Flexbox Column Center</div>
+      {/* 5. Flexbox Column Nested */}
+      <div className="h-screen flex flex-col justify-center items-center border-2 border-pink-500">
+        <div>🏀 5. Flex Column Nested</div>
       </div>
 
-      {/* Architect 6 - Fixed Screen Inset */}
-      <div className="relative min-h-[200px] border-2 border-purple-500">
+      {/* 6. Fixed + Inset Correctly Applied */}
+      <div className="relative border-2 border-purple-500" style={{ height: "100vh" }}>
         <div className="fixed inset-0 flex justify-center items-center">
-          <div>🏀 Fixed Inset Center</div>
+          🏀 6. Fixed Inset (true fullscreen)
         </div>
       </div>
 
-      {/* Architect 7 - Inline Block */}
-      <div className="min-h-[200px] text-center flex justify-center items-center border-2 border-cyan-500">
-        <span className="inline-block">🏀 Inline Block Center</span>
+      {/* 7. Inline-Block + Text-Center (wrapped inside centered flex) */}
+      <div className="h-screen flex justify-center items-center text-center border-2 border-cyan-500">
+        <span className="inline-block">🏀 7. Inline Block Inside Flex</span>
       </div>
 
-      {/* Architect 8 - Vanilla CSS Flex */}
-      <div style={{
-        height: "200px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "2px solid orange"
-      }}>
-        🏀 Vanilla CSS Flex Center
+      {/* 8. Raw Inline CSS Only */}
+      <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", border: "2px solid orange" }}>
+        🏀 8. Vanilla Flex CSS (again)
       </div>
 
-      {/* Architect 9 - Fixed Width Container */}
-      <div className="min-h-[200px] flex justify-center items-center border-2 border-lime-500">
-        <div className="w-[300px] text-center">🏀 Fixed Width Box Center</div>
+      {/* 9. Fixed width + mx-auto inside full-page flex */}
+      <div className="h-screen flex items-center border-2 border-lime-500">
+        <div className="w-[300px] mx-auto text-center">🏀 9. Fixed Width + mx-auto</div>
       </div>
 
-      {/* Architect 10 - HTML <center> (Bad Boy) */}
-      <div className="min-h-[200px] bg-gray-700 border-2 border-white">
-        <center style={{ height: "200px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-          🏀 Center Tag Cheat
-        </center>
+      {/* 10. Body-filling wrapper with manual padding */}
+      <div className="border-2 border-white" style={{ height: "100vh", paddingTop: "calc(50vh - 1rem)", textAlign: "center" }}>
+        🏀 10. Padding Top Manual Center
       </div>
 
     </div>
